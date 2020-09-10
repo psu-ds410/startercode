17a97d0821f9a36720f0568b0e10dbaf5f408b86
Daniel Kifer <dkifer@cse.psu.edu>
2020-09-10 14:17:59 -0400

    .

---
+++ hw3/hw3.py
@@ -0,0 +1,40 @@
+# I collaborated with:
+#
+# 1)
+# 2) 
+# ...
+#
+
+
+from dask import delayed
+from dask.distributed import Client
+from typing import List, Dict, Tuple, Any
+import re
+
+def tokenize(line: str) -> List[str]:
+    """ Splits a line into words """
+    return re.split("\W+", line.strip())
+
+
+def sortfile(f: str) -> List[str]:
+    """ Returns an array consisting of the sorted words in f"""
+    with open(f, "r") as infile:
+        words = [word for line in infile.readlines() for word in tokenize(line)]
+        words.sort()
+    return words
+
+
+def mergesort(file_list: List[str]) -> Tuple[Any, List[str]]:
+    """ Performas a parallelized merge sort with branching factor 2 over the files in file_list 
+    
+    Input: 
+       file_list: list of file names
+
+    Output:
+       a tuple. The first part of the tuple is the delayed object for the computation, the second part is a list
+       of the sorted words
+
+    Note:
+       you must use the futures api and merge files as results become available
+    """
+    pass
