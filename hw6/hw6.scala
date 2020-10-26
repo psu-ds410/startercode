5313e088fb32a39143f0ed00d8e630e7fd89cdac
Daniel Kifer <dkifer@cse.psu.edu>
2020-10-26 19:04:42 -0400

    hw6

---
+++ hw6/hw6.scala
@@ -0,0 +1,23 @@
+case class Neumaier(sum: Double, c: Double)
+
+object HW {
+
+   def q1_top_two(x: Int, y: Int, z:Int) = {
+      (1,1)
+   }
+
+   def q2_interpolation(name: String, age: Int) = {
+      ""
+   }
+
+   def q3_polynomial(arr: Seq[Double]) = {
+      0.0
+   }
+
+   def q4_application(x: Int, y: Int, z: Int)(f: (Int, Int) => Int) = {
+      1
+   }
+
+   // create the rest of the functions yourself
+
+}
